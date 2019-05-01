5545e2334c8f39ec0e9895ab3aeabdf2abcd9422
Frumpops <C17753589@mydit.ie>
2019-05-02 00:47:59 +0100

    roadmap for tomorrow

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -13,6 +13,13 @@ public class Main
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		main.startUI();
+		
+		//MealSelector, setUp JCombo Boxes so instaniate variables store consumables in list of Consumables
+		//Check calorie does not exceed 800
+		//Loop over food prep descriptions with 2 second wait between each 
+		//Plop out food
+
+		
 	}
 }
