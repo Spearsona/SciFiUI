6cd23f374938ccaa2499358824be014ed5114ed8
Spearsona <C17753589@mydit.ie>
2019-05-02 15:35:42 +0100

    creating meal selection class

---
+++ java/src/ie/tudublin/MealSelector.java
@@ -0,0 +1,20 @@
+package ie.tudublin;
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+
+public class MealSelector extends PApplet{
+    
+    public ArrayList<Consumable> allConsumables = new  ArrayList<Consumable>();
+
+    public ArrayList<Beverage> beveList = new  ArrayList<Beverage>();
+
+    public Beverage[] beverages = new Beverage[3];
+
+    
+
+    
+    
+    
+} 
